055c0bcdfd061069984966754a86fb3fe37542fd
Nesh-Rasikfareed <rasik.fareed@neshinc.com>
2017-10-16 15:02:56 +0530

    first commit

---
+++ app/src/main/java/com/netlify/rasikfareed/speedometer/MainActivity.java
@@ -0,0 +1,320 @@
+package com.netlify.rasikfareed.speedometer;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cardiomood.android.controls.gauge.SpeedometerGauge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.netlify.rasikfareed.speedometer.LocationService.MY_ACTION;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private boolean bound = false;
+
+    private SpeedometerGauge speedometer;
+    public static final int MULTIPLE_PERMISSION_REQUEST = 43;
+    String TAG = "MainActivity";
+    LocationService myService;
+    static boolean status;
+    LocationManager locationManager;
+    static TextView dist, time, speed;
+    Button start, pause, stop;
+    static long startTime, endTime;
+    ImageView image;
+    static ProgressDialog locate;
+    static int p = 0;
+
+    private ServiceConnection sc = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            LocationService.LocalBinder binder = (LocationService.LocalBinder) service;
+            myService = binder.getService();
+            status = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            status = false;
+        }
+    };
+
+    void bindService() {
+        if (status == true)
+            return;
+        Intent i = new Intent(getApplicationContext(), LocationService.class);
+        bindService(i, sc, BIND_AUTO_CREATE);
+        status = true;
+        startTime = System.currentTimeMillis();
+    }
+
+    void unbindService() {
+        if (status == false)
+            return;
+        Intent i = new Intent(getApplicationContext(), LocationService.class);
+        unbindService(sc);
+        status = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(br, new IntentFilter(MY_ACTION));
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(br);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (status == true)
+            unbindService();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (status == false)
+            super.onBackPressed();
+        else
+            moveTaskToBack(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        speedometer = (SpeedometerGauge) findViewById(R.id.speedometer);
+
+        dist = (TextView) findViewById(R.id.distancetext);
+        time = (TextView) findViewById(R.id.timetext);
+        speed = (TextView) findViewById(R.id.speedtext);
+
+        start = (Button) findViewById(R.id.start);
+        pause = (Button) findViewById(R.id.pause);
+        stop = (Button) findViewById(R.id.stop);
+
+        image = (ImageView) findViewById(R.id.image);
+
+        speedometer.setLabelConverter(new SpeedometerGauge.LabelConverter() {
+            @Override
+            public String getLabelFor(double progress, double maxProgress) {
+                return String.valueOf((int) Math.round(progress));
+            }
+        });
+        // configure value range and ticks
+        speedometer.setMaxSpeed(300);
+        speedometer.setMajorTickStep(30);
+        speedometer.setMinorTicks(2);
+
+        // Configure value range colors
+        speedometer.addColoredRange(30, 140, Color.GREEN);
+        speedometer.addColoredRange(140, 180, Color.YELLOW);
+        speedometer.addColoredRange(180, 400, Color.RED);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //The method below checks if Location is enabled on device or not. If not, then an alert dialog box appears with option
+                //to enable gps.
+                //isPermissionGranted();
+              //  checkGps();
+           if(requestPermission(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION)){
+               checkGps();
+           }
+                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                    return;
+                }
+
+
+                if (status == false)
+                    //Here, the Location Service gets bound and the GPS Speedometer gets Active.
+                    bindService();
+                locate = new ProgressDialog(MainActivity.this);
+                locate.setIndeterminate(true);
+                locate.setCancelable(false);
+                locate.setMessage("Getting Location...");
+                locate.show();
+                start.setVisibility(View.GONE);
+                pause.setVisibility(View.VISIBLE);
+                pause.setText("Pause");
+                stop.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pause.getText().toString().equalsIgnoreCase("pause")) {
+                    pause.setText("Resume");
+                    p = 1;
+
+                } else if (pause.getText().toString().equalsIgnoreCase("Resume")) {
+                    checkGps();
+                    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        Toast.makeText(MainActivity.this, "GPS is not enabled in your device", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    pause.setText("Pause");
+                    p = 0;
+
+                }
+            }
+        });
+
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (status == true)
+                    unbindService();
+                start.setVisibility(View.VISIBLE);
+                pause.setText("Pause");
+                pause.setVisibility(View.GONE);
+                stop.setVisibility(View.GONE);
+                p = 0;
+            }
+        });
+    }
+
+
+    //This method leads you to the alert dialog box.
+    void checkGps() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+
+            showGPSDisabledAlertToUser();
+        }
+    }
+
+
+    //This method configures the Alert Dialog box.
+    private void showGPSDisabledAlertToUser() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Enable GPS to use application")
+                .setCancelable(false)
+                .setPositiveButton("Enable GPS",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    public boolean requestPermission(String... permissions) {
+        final List<String> permissionsList = new ArrayList<>();
+
+        for (String perm : permissions) {
+            addPermission(permissionsList, perm);
+        }
+
+        if (permissionsList.size() > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),MULTIPLE_PERMISSION_REQUEST);
+            }
+            else
+                ActivityCompat.requestPermissions(this, permissionsList.toArray(new String[permissionsList.size()]),MULTIPLE_PERMISSION_REQUEST);
+            return false;
+        } else
+            return true;
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!shouldShowRequestPermissionRationale(permission))
+                        return false;
+                }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+    switch (requestCode) {
+        case MULTIPLE_PERMISSION_REQUEST: {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                checkGps();
+                Toast.makeText(MainActivity.this,"Permission granted",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this,"No permission granted",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+    }
+
+    private BroadcastReceiver br = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                speedometer.setSpeed(bundle.getDouble("Speed"));
+            }
+        }
+    };
+
+
+}
